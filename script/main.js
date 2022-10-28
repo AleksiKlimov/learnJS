212b35723bced314d957f77be4cbb8af18dce3e1
AleksiKlimov <leha.kurdyukov@internet.ru>
2022-10-28 20:01:00 +0300

    new functuionality

--- script/main.js
+++ script/main.js
@@ -1,70 +1,121 @@
-import { showShips, dragNDrop } from "./dragNDrop.js";
-import {
-  randomPositionShip,
-  randomOrientation,
-  randomCoordinates,
-} from "./getRandomPosShips.js";
-//=====================================================
+const $fieldHuman = document.querySelector(".seabattle__sea-human");
+const $fieldComputer = document.querySelector(".seabattle__sea-computer");
+const $createElement = document.createElement("div");
+const $visualShipEscadraHuman = document.querySelector(
+  ".seabattle__escadra-human"
+);
+const $visualShipEscadraComputer = document.querySelector(
+  ".seabattle__escadra-computer"
+);
 
-//=====================================================================
-//gamesField PC and gamer
-const humanField = document.querySelector(".seabattle__sea-human");
-const computerField = document.querySelector(".seabattle__sea-computer");
+const flagTrue = true;
+const flagFalse = false;
 
-//create default obj================================
-function createCell() {
-  return { x: 0, y: 0, direction: null, hits: 0, ship: false };
-}
-//array counter and length ships==========================================
-const escadraArray = () => {
-  const arr = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
-  return arr;
+const createDefaultObj = () => {
+  return {
+    x: 0,
+    y: 0,
+    direction: null,
+    ship: false,
+  };
 };
-//function create matrix with defaults objects=================================
-const createInterfaceField = (battleField) => {
-  const mainArr = [];
-  for (let y = 0; y <= 9; y++) {
-    const innerArr = [];
-    mainArr.push(innerArr);
-    for (let x = 0; x <= 9; x++) {
-      const $filedCell = document.createElement("div");
-      $filedCell.classList.add("cube");
-      $filedCell.dataset.y = y;
-      $filedCell.dataset.x = x;
-      const cell = createCell();
-      // newDiv.classList.add("cube");
-      cell.y = y;
-      cell.x = x;
-      cell.div = $filedCell;
 
-      battleField.append($filedCell);
-      mainArr[y][x] = cell;
+const createDefaultEscadra = (() => {
+  return [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
+})();
+
+const createBattleField = ($html, flag) => {
+  const outerMatrix = [];
+  for (let y = 0; y < 10; y++) {
+    const innerMatrixRow = [];
+    for (let x = 0; x < 10; x++) {
+      //html=================================
+      const $element = document.createElement("div");
+      $element.dataset.y = y;
+      $element.dataset.x = x;
+      flag
+        ? $element.classList.add("cell__gamer")
+        : $element.classList.add("cell__computer");
+      //obj==================================
+      const defaultObj = createDefaultObj();
+      defaultObj.y = y;
+      defaultObj.x = x;
+      defaultObj.elem = $element;
+      $html.append($element);
+      innerMatrixRow.push(defaultObj);
     }
+    outerMatrix.push(innerMatrixRow);
   }
-  return mainArr;
+  // console.log(outerMatrix);
+  return outerMatrix;
 };
-//event listener main buttons games ====================================================
-document.addEventListener("click", (event) => {
-  if (event.target.dataset.random) {
-    //RANDOM================================================
-    humanField.innerHTML = "";
-    const mainArrHuman = createInterfaceField(humanField);
-    const escadraHuman = showShips(escadraArray(), true);
-    randomPositionShip(escadraHuman, mainArrHuman);
-    // dragNDrop(escadraHuman, mainArrHuman);
-  } else if (event.target.dataset.manual) {
-    //MANUAL=================================================
-    humanField.innerHTML = "";
-    const mainArrHuman = createInterfaceField(humanField);
-    const escadraHuman = showShips(escadraArray(), true);
-    dragNDrop(escadraHuman, mainArrHuman);
-  } else if (event.target.dataset.start) {
-    //START==========================================
 
-    computerField.innerHTML = "";
-    const mainArrComputer = createInterfaceField(computerField);
-    const escadraComputer = showShips(escadraArray(), false);
-    const positionFullShip = randomPositionShip(escadraComputer);
+const createVisualShips = (escadraShips, $html, flag) => {
+  const arrOuterShips = [];
+
+  for (let y = 0; y < escadraShips.length; y++) {
+    const arrInnerShips = [];
+
+    const $shipContainer = document.createElement("div");
+    flag
+      ? $shipContainer.classList.add("cell__container-human")
+      : $shipContainer.classList.add("cell__container-computer");
+
+    for (let x = 0; x < escadraShips[y]; x++) {
+      //html element================================================
+      //===============================
+      const $elem = document.createElement("div");
+      $elem.dataset.y = y;
+      $elem.dataset.x = x;
+      flag
+        ? $elem.classList.add("ship__gamer")
+        : $elem.classList.add("ship__computer");
+      //create obj ============================================================
+      //====================================
+      const defaultObj = createDefaultObj();
+      defaultObj.y = y;
+      defaultObj.x = x;
+      defaultObj.elem = $elem;
+      defaultObj.direction = defaultObj.ship = true;
+      $shipContainer.append($elem);
+      arrInnerShips.push(defaultObj);
+    }
+    $html.append($shipContainer);
+    arrOuterShips.push(arrInnerShips);
   }
-});
-export { escadraArray, createCell };
+  console.log(arrOuterShips);
+  return arrOuterShips;
+};
+//==================================================================
+//manual
+const manualVariant = createBattleField($fieldHuman, flagTrue);
+const manualVisualShips = createVisualShips(
+  createDefaultEscadra,
+  $visualShipEscadraHuman,
+  flagTrue
+);
+
+//===================================================================
+//random
+// const randomVarian = createBattleField($fieldHuman, flagTrue);
+// const randomHiddenShips = createVisualShips(
+//   createDefaultEscadra,
+//   $visualShipEscadraHuman,
+//   flagTrue
+// );
+//====================================================================
+//start
+const fieldForComputer = createBattleField($fieldComputer, flagFalse);
+const escadraHiddenForComputer = createVisualShips(
+  createDefaultEscadra,
+  $visualShipEscadraComputer,
+  flagFalse
+);
+//=====================================================================
+
+document.onclick = (event) => {
+  if (event.target.dataset.manual) {
+  } else if (event.target.dataset.random) {
+  } else if (event.target.dataset.start) {
+  }
+};
